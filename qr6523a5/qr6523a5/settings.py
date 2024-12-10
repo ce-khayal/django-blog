3c45a3e1a0426e585aeede9406c82c37208b24cb
ce-khayal <cavasir23@gmail.com>
2024-12-10 16:36:04 +0400

    Updated ALLOWED_HOSTS for Render

--- qr6523a5/qr6523a5/settings.py
+++ qr6523a5/qr6523a5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a_a5rydb$#1w$=klh(ihs)dlqw%#c*(b8(45+)w&xc_edmt=ir
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['serbestis.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
