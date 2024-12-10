51e6c966246acce77ec306b4a75c7f96e6fdf11b
ce-khayal <cavasir23@gmail.com>
2024-12-10 16:59:42 +0400

    Fixed wsgi path

--- qr6523a5/qr6523a5/settings.py
+++ qr6523a5/qr6523a5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a_a5rydb$#1w$=klh(ihs)dlqw%#c*(b8(45+)w&xc_edmt=ir
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://django-blog-ga3v.onrender.com']
+ALLOWED_HOSTS = ['https://django-blog-ga3v.onrender.com','127.0.0.1']
 
 
 # Application definition
