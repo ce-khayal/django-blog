935ad37a8ab7d2e96101cebded93efd5f490f33b
ce-khayal <cavasir23@gmail.com>
2024-12-12 23:14:25 +0400

    Update wsgi.py

--- qr6523a5/qr6523a5/wsgi.py
+++ qr6523a5/qr6523a5/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for qr6523a5 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# Layihənin əsas qovluğunu əlavə edin
+project_home = 'qr6523a5/qr6523a5'  # Əsas layihə qovluğunuz
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Virtual mühiti aktivləşdirin (əgər varsa)
+activate_this = 'myvenv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Django layihəsinin settings faylını göstərin
+os.environ['DJANGO_SETTINGS_MODULE'] = 'qr6523a5.settings'  # Layihə adını uyğunlaşdırın
+
+# Django WSGI tətbiqini yaradın
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr6523a5.settings')
-
 application = get_wsgi_application()
